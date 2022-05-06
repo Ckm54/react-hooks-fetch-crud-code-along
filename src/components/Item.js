82e732e825d48c89598ff3017f87492cdbb080ec
Collins <mutaic84@gmail.com>
2022-05-06 11:49:19 +0300

    handle cicking delete button

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Item({ item, onUpdateItem }) {
+function Item({ item, onUpdateItem, onDeleteItem }) {
 
   function handleAddToCardClick() {
     fetch(`http://localhost:4000/items/${item.id}`, {
@@ -15,6 +15,14 @@ function Item({ item, onUpdateItem }) {
     .then((res) => res.json())
     .then((updatedItem) => onUpdateItem(updatedItem))
   }
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+    .then((res) => res.json())
+    .then(() => onDeleteItem(item))
+  }
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
@@ -23,7 +31,7 @@ function Item({ item, onUpdateItem }) {
       onClick={handleAddToCardClick}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={handleDeleteClick}>Delete</button>
     </li>
   );
 }
