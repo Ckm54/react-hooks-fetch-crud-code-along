186a70254a6681586186fc0d94940aeb2ce7d96d
Collins <mutaic84@gmail.com>
2022-05-06 11:37:52 +0300

    pass updated item to shoppingList component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem }) {
 
   function handleAddToCardClick() {
     fetch(`http://localhost:4000/items/${item.id}`, {
@@ -13,7 +13,7 @@ function Item({ item }) {
       }),
     })
     .then((res) => res.json())
-    .then((updatedItem) => console.log(updatedItem))
+    .then((updatedItem) => onUpdateItem(updatedItem))
   }
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
